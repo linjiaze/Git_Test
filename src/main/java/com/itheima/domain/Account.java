b501ffd6e77b2a82939d7b21fd43db43d4c770d5
linjz <linjz234@163.com>
2019-06-12 11:33:26 +0800

    update

--- src/main/java/com/itheima/domain/Account.java
+++ src/main/java/com/itheima/domain/Account.java
@@ -2,4 +2,30 @@ package com.itheima.domain;
 
 public class Account {
     private String accountId;
+    private String password;
+    private Double remaining;
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Double getRemaining() {
+        return remaining;
+    }
+
+    public void setRemaining(Double remaining) {
+        this.remaining = remaining;
+    }
 }
